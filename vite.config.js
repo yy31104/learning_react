c64774dbb190b4ab2fc3aff47b0d7bdb2172ee23
yy31104 <147130934+yy31104@users.noreply.github.com>
2026-02-14 11:12:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPOSITORY_NAME",
+  base: "/learning_react",
   plugins: [react()],
 })
